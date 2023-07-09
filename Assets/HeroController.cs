674beb3d8ecec6d4cd786fb01f71dea91d7dde83
Michael Ebneth <michael.ebneth@gmx.de>
2023-07-09 11:14:53 +0200

    feat: :sparkles: add movement to hero

--- Assets/HeroController.cs
+++ Assets/HeroController.cs
@@ -7,6 +7,7 @@ public class HeroController : MonoBehaviour
     Animator animator;
     public Transform startPoint;
     public Transform endPoint;
+    public Transform destination;
     public float movementSpeed = 2f;
 
     private float t = 0f;
@@ -16,32 +17,56 @@ public class HeroController : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         animator.SetBool("isWalking", true);
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        MoveToEndPoint();
+        // MoveToEndPoint();
+        transform.position = Vector3.Lerp(transform.position, destination.position, Time.deltaTime * movementSpeed);
+        var dist = Vector3.Distance(transform.position, destination.position);
+        print(dist);
+        if (dist <= 1)
+        {
+            // Changes position
+            if (destination == startPoint)
+            {
+                destination = endPoint;
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                destination = startPoint;
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
     }
 
     void MoveToStartPoint()
     {
-
+        animator.SetBool("isWalking", true);
+        // Move the NPC from start to end point
+        t += Time.deltaTime * movementSpeed;
+        transform.position = Vector3.Lerp(transform.position, startPoint.position, t);
     }
     void MoveToEndPoint()
     {
         animator.SetBool("isWalking", true);
         // Move the NPC from start to end point
-        t += Time.deltaTime / movementSpeed;
-        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, 0.05f);
-        print(t);
-        // If the NPC has reached the end point, reverse the direction
-        if (t >= 1f)
+        t += Time.deltaTime * movementSpeed;
+        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t);
+
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger");
+        if (collision.gameObject.CompareTag("Conversation Spot"))
         {
-            var temp = startPoint;
-            startPoint = endPoint;
-            endPoint = temp;
-            t = 0f;
+            print("Conversation Spot");
+            // MoveToStartPoint();
+            // Handle collision with the boundary here
+            // For example, you can reset the player's position or apply some other behavior
         }
     }
 }
